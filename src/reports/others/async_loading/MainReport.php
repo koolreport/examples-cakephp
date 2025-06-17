e18e48543f888287f296ff2955fe49ea6bf94db8
bright <apple@MacBook-Pro-cua-Apple-2.local>
2025-06-17 14:17:44 +0700

    delete jquery chartjs reports

--- src/reports/others/async_loading/MainReport.php
+++ src/reports/others/async_loading/MainReport.php
@@ -6,7 +6,7 @@ require "TopCustomersReport.php";
 class MainReport extends \koolreport\KoolReport
 {
     
-    use \koolreport\clients\jQuery;
+
     use \koolreport\core\SubReport;
     
     function settings()
